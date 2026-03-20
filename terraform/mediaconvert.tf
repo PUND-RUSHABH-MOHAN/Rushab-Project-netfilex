7847cf8f8c705f144b548a2acbb4e35e6922128d
PUND RUSHABH MOHAN <rushabpund@gmail.com>
2026-03-20 12:27:43 +0530

    Add placeholder for MediaConvert Terraform configuration

---
+++ terraform/mediaconvert.tf
@@ -0,0 +1,6 @@
+# This is a placeholder - MediaConvert doesn't have Terraform resource management
+# Jobs are created by Lambda #1 at runtime
+
+output "mediaconvert_info" {
+  value = "MediaConvert jobs are created dynamically by Lambda #1"
+}
